c81b62b9dc1f1cd6d93e0279e8262f6e1cfe1de3
조기범 <akfmzh1207@naver.com>
2023-02-24 14:30:42 +0900

    설문조사 확인 폼 개발중

---
+++ src/pages/Manager/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const BoxFlex = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Box = styled.div`
+  width: 1000px;
+  height: 500px;
+  background-color: #c0c0c0;
+  margin-top: 40px;
+`;

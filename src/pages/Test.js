4da21cd984df26380734d4d1f607532fa4eb8f0f
조기범 <akfmzh1207@naver.com>
2023-03-08 01:55:34 +0900

    테스트중

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -79,10 +79,10 @@ const Test = () => {
   //   }
   // };
   const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
-  const PROX = window.location.hostname === "localhost" ? "" : "/prox";
+
   const GetUserId = async () => {
     await axios
-      .post(`${PROX}/v1/participant`)
+      .post(`${PROXY}/v1/participant`)
       .then((response) => {
         console.log(response.data.participantId);
         setOpj(response.data.participantId);
